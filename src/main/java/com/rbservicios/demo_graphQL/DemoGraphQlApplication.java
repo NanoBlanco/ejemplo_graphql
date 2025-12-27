b199a3e71f0bae88218b8b354c91b9faa3b21acf
= <=>
2025-12-27 17:38:43 -0300

    Primer Commit, estructura basica

---
+++ src/main/java/com/rbservicios/demo_graphQL/DemoGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.rbservicios.demo_graphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbservicios/demo_graphQL/DemoGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbservicios.demo_graphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
